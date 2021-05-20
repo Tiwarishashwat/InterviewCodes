fcd295239dc646bcdc287806e7944e2622942584
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2021-05-20 18:11:52 +0530

    edited

--- CommanElements.java
+++ CommanElements.java
@@ -3,24 +3,31 @@ class Solution
     ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
     {
         // code here 
+        // Declare an Array list to store results
+        ArrayList<Integer> arr = new ArrayList<>();
         int i=0,j=0,k=0;
-        ArrayList<Integer> res = new ArrayList<>();
         while(i<n1 && j<n2 && k<n3)
         {
-            if(A[i]==B[j]) 
+            if(A[i]<B[j]) i++;
+            else if(A[i]>B[j]) j++;
+            else
             {
-                while(k<n3 && C[k]<A[i]) k++;
-                if(k<n3 && A[i]==C[k])
+                // This is for checking duplicacy
+                if(i>0 && A[i]==A[i-1])
                 {
-                    if(res.size()==0 || res.get(res.size()-1)!=A[i]) res.add(A[i]);
-                    k++;
+                    i++;
+                    continue;
                 }
-                i++;
-                j++;
+                // Now we will search the common element in third array!
+                // k<n3 avoids array index out of bound exception
+                while(k<n3 && C[k]<B[j]) k++;
+                if(k<n3 && C[k]==B[j])
+                {
+                    arr.add(C[k]);
+                }
+                i++;j++;
             }
-            else if(A[i]<B[j]) i++;
-            else if(B[j]<A[i]) j++;
         }
-        return res;
+        return arr;
     }
 }
