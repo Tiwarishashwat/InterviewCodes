5e7a0323ffdd46bf2fdbdee1efdfe95ad936bd87
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2021-06-20 11:25:58 +0530

    Update RearrangeArrayAlternatively.java

--- RearrangeArrayAlternatively.java
+++ RearrangeArrayAlternatively.java
@@ -4,27 +4,31 @@ class Solution{
     // n: size of array
     //Function to rearrange  the array elements alternately.
     public static void rearrange(int arr[], int n){
-    // Your code here
-    int mini=0;
-    int maxi=n-1;
-    int val=arr[n-1]+1;
-    for(int i=0;i<n;i++)
-    {
-      if(i%2==0)
-      {
-          arr[i]=val*(arr[maxi]%val)+arr[i];
-          maxi--;
-      }
-      else
-      {
-          arr[i]=val*(arr[mini]%val)+arr[i];
-          mini++;
-      }
-    }
-    for(int i=0;i<n;i++)
-    {
-        arr[i]=arr[i]/val;
+        
+        // Your code here
+        int j=0,k=n-1;
+        int key=arr[n-1]+1;
+        for(int i=0;i<n;i++)
+        {
+            // even should be max
+            if(i%2==0)
+            {
+                arr[i]=((arr[k]%key)*key)+arr[i];
+                k--;
+            }
+            // odd should be min
+            else
+            {
+                arr[i] = ((arr[j]%key)*key)+arr[i];
+                j++;
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            arr[i]/=key;
+        }
+
     }
     
-    }
 }
