79e98fad5ac1e6d07b9b39ac865ee765647e1666
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2021-06-23 20:50:01 +0530

    Update LargeFactorial.java

--- LargeFactorial.java
+++ LargeFactorial.java
@@ -1,27 +1,39 @@
+//User function Template for Java
+
 class Solution {
-    static ArrayList<Integer> factorial(int n){
-        
-        ArrayList<Integer> res = new ArrayList<>();
-        res.add(0,1);
-        int res_size = 1;
-        for(int x = 2; x <= n; x++)
+    static ArrayList<Integer> factorial(int n)
         {
-            int carry = 0; 
-        for(int i=res_size-1;i>=0;i--)
+        // declare an arrayList
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int size=0,c=0;
+        // Adding 1 at 0th index
+        result.add(0,1);
+        // Updating size
+        size=1;
+        // Decalre a variable to traverse numbers from 2 to n
+        int val=2;
+        while(val<=n)
         {
-            int prod = res.get(i) * x + carry;
-            res.set(i,prod % 10); // Store last digit of 'prod' in res[]
-            carry = prod/10; // Put rest in carry
-        }
- 
-        // Put carry in res and increase result size
-        while (carry!=0)
+            // Traverse array list from right to left
+            for(int i=size-1;i>=0;i--)
             {
-                res.add(0,carry % 10);
-                carry = carry / 10;
-                res_size++;
+                // Update value in arrayList
+                int temp=result.get(i)*val + c;
+                // Store the last digit at index and add remaining to carry
+                result.set(i,temp%10);
+                // update carry
+                c=temp/10;
             }
+            // insert carry digit by digit to the begining of the ArrayList
+            while(c!=0)
+            {
+                result.add(0,c%10);
+                c=c/10;
+                size++;
+            }
+            val++;
         }
-            return res;
+       return result;
         }
     }
+    
