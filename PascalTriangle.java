a4aa41eec79e35623122e6af4736f4bc657819db
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2021-07-22 01:26:21 +0530

    Update PascalTriangle.java

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -2,27 +2,28 @@ class Solution
 {
     public List<List<Integer>> generate(int numRows) 
     {
-//         Declare an array List for storing the result
+        // Declare list of list for storing results
         List<List<Integer>> pascalTriangle = new ArrayList<List<Integer>>();
-        pascalTriangle.add(0,new ArrayList<>(Arrays.asList(1)));
-//         Variable to keep track of each row
-        int i=2;
-//         Calculating value of each row of pascal triangle
-        while(i<=numRows)
+        pascalTriangle.add(0,new ArrayList<Integer>(Arrays.asList(1)));
+        
+        int row =2;
+        
+        while(row<=numRows)
         {
-            int start=0,end=i-1;
-            List<Integer> temp = new ArrayList<Integer>();
-            temp.add(start,1);
-            start++;
-            for(int j=start;j<end;j++)
+            List<Integer> temp = new ArrayList<>();
+            temp.add(0,1);
+            int start=1,end=row-1;
+            for(int i=start;i<end;i++)
             {
-                int sum = pascalTriangle.get(i-2).get(j-1)+pascalTriangle.get(i-2).get(j);
-                temp.add(j,sum);
+                int sum = pascalTriangle.get(row-2).get(i-1)+pascalTriangle.get(row-2).get(i);
+                temp.add(i,sum);
             }
             temp.add(end,1);
             pascalTriangle.add(temp);
-            i++;
+            row++;
         }
         return pascalTriangle;
     }
 }
+
+
