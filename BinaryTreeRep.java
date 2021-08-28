b83d0d83907995703b2d21e59a8b2a383d91e6c3
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2021-08-28 13:08:35 +0530

    Create BinaryTreeRep.java

---
+++ BinaryTreeRep.java
@@ -0,0 +1,19 @@
+public class BinaryTreeRep
+{
+	public static void main(String[] args) {
+	    Node root = new Node(1);
+	    root.left = new Node(2);
+	    root.right = new Node(3);
+	    root.left.left = new Node(4);
+	}
+}
+class Node
+{
+        int data;
+        Node left;
+        Node right;
+        public Node(int data)
+        {
+            this.data=data;
+        }
+}
