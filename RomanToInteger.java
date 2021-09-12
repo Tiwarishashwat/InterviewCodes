02a09cc0ffd409d5626a50aebbc85d511e1ac736
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2021-09-12 17:41:40 +0530

    Update RomanToInteger.java

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,37 +1,30 @@
 class Solution {
     // Finds decimal value of a given roman numeral
     public int romanToDecimal(String s) {
-    Map<Character,Integer> roman = new HashMap<Character,Integer>();
-
-    roman.put('I', 1);
-    roman.put('V', 5);
-    roman.put('X', 10);
-    roman.put('L', 50);
-    roman.put('C', 100);
-    roman.put('D', 500);
-    roman.put('M', 1000);
-
-    int sum = 0;
-    int n = s.length();
-     
-    for(int i = 0; i < n; i++)
-    {
-         
-        // If present value is less than next value,
-        // subtract present from next value and add the
-        // resultant to the sum variable.
-        if (i != n - 1 && roman.get(s.charAt(i)) <
-                          roman.get(s.charAt(i + 1)))
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        
+        int n=s.length();
+        int output=0;
+        for(int i=0;i<n;i++)
         {
-            sum += roman.get(s.charAt(i + 1)) -
-                   roman.get(s.charAt(i));
-            i++;
+         if(i<n-1 && map.get(s.charAt(i))<map.get(s.charAt(i+1)))
+         {
+             output+= map.get(s.charAt(i+1))-map.get(s.charAt(i));
+             i++;
+         }
+         else
+         {
+             output+=map.get(s.charAt(i));
+         }
         }
-        else
-        {
-            sum += roman.get(s.charAt(i));
-        }
-    }
-    return sum;
+        return output;
+
     }
 }
