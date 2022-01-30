788ac6e42db045a562a1da28f493a97c0d605b98
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2022-01-30 20:55:45 +0530

    Update Minimum_number_of_jumps.java

--- Minimum_number_of_jumps.java
+++ Minimum_number_of_jumps.java
@@ -1,22 +1,27 @@
 class Solution{
     static int minJumps(int[] arr){
         // your code here
-     int max=0,jump=0,halt=0;
-     if(arr[0]==0) return -1;
-     for(int i=0;i<arr.length-1;i++)
-     {
-         max=Math.max(max,i+arr[i]);
-         if(i==halt)
-         {
-             halt=max;
-             jump++;
-         }
-     }
-     
-     if(halt>=arr.length-1)
-     {
-         return jump;
-     }
-     return -1;
+        if(arr.length==1)
+            return 0;
+            
+        if(arr[0]==0)
+            return -1;
+        
+        int jump=0,halt=0;
+        int maxDist=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i]+i>=maxDist)
+                maxDist=arr[i]+i;
+            
+            if(i==halt)
+            {
+                halt=maxDist;
+                jump++;
+                if(halt>=arr.length-1)
+                    break;
+            }
+        }
+        return (halt>=arr.length-1)?jump:-1;
     }
 }
