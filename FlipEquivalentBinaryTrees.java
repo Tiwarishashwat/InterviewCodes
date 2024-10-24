c13dc2aa654a2b6ede6a3d788716a0d016995036
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2024-10-24 09:06:02 +0530

    Create FlipEquivalentBinaryTrees.java

---
+++ FlipEquivalentBinaryTrees.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        //base cases
+        if(root1 == null && root2==null){
+            return true;
+        }
+        if(root1 == null || root2==null){
+            return false;
+        }
+        if(root1.val!=root2.val){
+            return false;
+        }
+        //orignal
+        boolean isSame = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+        if(isSame){
+            return true;
+        }
+        //flip
+        return flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+    }
+}
